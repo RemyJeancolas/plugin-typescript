a53f9925cdba70cd871bd6fc29ac770ed5934371
Rémy Jeancolas <remy.jeancolas@illogeek.eu>
2016-09-30 09:48:41 -0400

    feat(plugin): Improve code to add compatibility with ES6

--- index.js
+++ index.js
@@ -10,7 +10,10 @@ module.exports = {
           * Always delay requires, otherwise your plugin will cause trouble
           * with db-migrates performance and generates issues to your users.
           */
-        require('typescript').register();
+        var typescriptModule = require('typescript');
+        if (typescriptModule && typescriptModule.register) {
+          typescriptModule.register();
+        }
 
         /**
           * Return value of this hook can be both, pure value or a promise.
